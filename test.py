843ad89aaefad87695ef911b487fc49f4e7555f8
Jonathan <jtduong22@gmail.com>
2020-01-16 13:19:11 -0800

    - reduced clutter and redudancy with parameters - added basic sorting ability - now prints header

--- test.py
+++ test.py
@@ -6,80 +6,63 @@ database = './Data/mhgu.db'
 # open database file
 conn = sql.connect(database)
 
-# parameters for which weapon details to show
-show_id = False
-show_creation_cost = False
-show_attack_melee = True
-show_attack_ranged = True
-show_element = True
-show_element_melee = True
-show_element_ranged = True
-show_defense = True
-show_sharpness = True
-show_affinity_melee = True
-show_affinity_ranged = True
-show_blunt = True
-show_balance = True
+# settings for which settings to show + padding for each
+parameters = {
+    "_id":[False, 8],
+    "creation_cost":[False, 5],
+    "attack_melee":[True,3],
+    "attack_ranged":[True,3],
+    "element":[True,11],
+    "element_melee":[True,3],
+    "element_ranged":[True,3],
+    "defense":[True,3],
+    "sharpness":[True,3],
+    "affinity_melee":[True,3],
+    "affinity_ranged":[True,3],
+    "blunt":[True,3],
+    "balance":[True,3]
+}
+padding = [("name", 25)]
 
 # generate command based on enabled parameters
-show_string = 'items.name'
-pw = 'palico_weapons.'
-padding = [25]
+show_string = 'items.name'.center(25)
 
 # go through parameter and add to string
-if show_id:
-    show_string += f', {pw}_id'
-    padding.append(8)
-if show_creation_cost:
-    show_string += f', {pw}creation_cost'
-    padding.append(5)
-if show_attack_melee:
-    show_string += f', {pw}attack_melee'
-    padding.append(3)
-if show_attack_ranged:
-    show_string += f', {pw}attack_ranged'
-    padding.append(3)
-if show_element:
-    show_string += f', {pw}element'
-    padding.append(11)
-if show_element_melee:
-    show_string += f', {pw}element_melee'
-    padding.append(3)
-if show_element_ranged:
-    show_string += f', {pw}element_ranged'
-    padding.append(3)
-if show_defense:
-    show_string += f', {pw}defense'
-    padding.append(3)
-if show_sharpness:
-    show_string += f', {pw}sharpness'
-    padding.append(3)
-if show_affinity_melee:
-    show_string += f', {pw}affinity_melee'
-    padding.append(3)
-if show_affinity_ranged:
-    show_string += f', {pw}affinity_ranged'
-    padding.append(3)
-if show_blunt:
-    show_string += f', {pw}blunt'
-    padding.append(2)
-if show_balance:
-    show_string += f', {pw}balance'
-    padding.append(2)
+for key in parameters.keys():
+    if parameters[key][0] == True:
+        show_string += f', palico_weapons.{key}'
+        padding.append((key, parameters[key][1]))
+
+is_order = True
+is_desc = True
+order_by = 'element_ranged'
+order_string = ''
+
+if is_order:
+    order_string = f"order by {order_by}"
+    if is_desc:
+        order_string += " desc"
+
 
 # generate entire command
-command = f"select {show_string} from items, palico_weapons where items._id = palico_weapons._id and palico_weapons.element='Paralysis' order by palico_weapons.element_ranged desc"
+command = f"select {show_string} from items, palico_weapons where items._id = palico_weapons._id and palico_weapons.element='Paralysis' {order_string}"
 
 # execute command and retrieve all weapons that meet parameter
 cursor = conn.execute(command)
 
-# print out formatted
+# temporary function that takes string and int and adds padding to the end
+print_formatted = lambda text, space: (print(f"{text.center(max(space, len(text)))}", end=" | "))
+
+# print out header
+for item in padding:
+    print_formatted(item[0], item[1])
+print()
+
+# print out formatted table
 for row in cursor:
-
-    # temporary function that takes string and int and adds padding to the end
-    print_formatted = lambda str, space: (print(f"{str:{space}}", end=" | "))
-
-    for i in zip(row, padding):
-        print_formatted(i[0], i[1])
+    for item in zip(row, padding):
+        text = str(item[0])
+        pad = max(item[1][1], len(item[1][0]))
+        print_formatted(text, pad)
 
     print()
